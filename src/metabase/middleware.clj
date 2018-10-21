2dad300ca89084876988920fd629b773ef8c18da
Abhinav Kumar Kushwaha <abhinav.k@phonepe.com>
2018-10-21 11:22:19 +0530

    Revert "comment issue"
    This reverts commit f4fbcd26363301d31f7578007dde88118275253a.

--- src/metabase/middleware.clj
+++ src/metabase/middleware.clj
@@ -240,8 +240,7 @@
    #_(public-key-pins-header)
    (when-not allow-iframes?
      ;; Tell browsers not to render our site as an iframe (prevent clickjacking)
-     ;; {"X-Frame-Options"                 "DENY"}
-     )
+     ;; {"X-Frame-Options"                 "DENY"})
    { ;; Tell browser to block suspected XSS attacks
     "X-XSS-Protection"                  "1; mode=block"
     ;; Prevent Flash / PDF files from including content from site.
