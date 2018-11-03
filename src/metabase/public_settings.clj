4f3c956a855216709c014a65e75a86b7ab25bcbd
Abhinav Kumar Kushwaha <abhinav.k@phonepe.com>
2018-11-03 13:00:54 +0530

    ipsator update ga code

--- src/metabase/public_settings.clj
+++ src/metabase/public_settings.clj
@@ -171,7 +171,7 @@
    :enable_nested_queries (enable-nested-queries)
    :enable_xrays          (enable-xrays)
    :engines               ((resolve 'metabase.driver/available-drivers))
-   :ga_code               "UA-60817802-1"
+   :ga_code               "UA-113474819-3"
    :google_auth_client_id (setting/get :google-auth-client-id)
    :has_sample_dataset    (db/exists? 'Database, :is_sample true)
    :hide_embed_branding   (metastore/hide-embed-branding?)
